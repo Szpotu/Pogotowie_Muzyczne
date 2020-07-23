89da3e401c861010d467a8e7e3d9ca2877619393
Szpotsu <szpots.dev@gmail.com>
2020-07-23 03:19:15 +0200

    Added hiding Nav, few css corrections

--- main_js/hide_nav_scroll.js
+++ main_js/hide_nav_scroll.js
@@ -1,1 +1,22 @@
-let actualPosition = window.pageYOffset;
+let prevScrollPosition = window.pageYOffset; // global var - previous scroll px
+
+
+window.onscroll = () => {
+    let actualScrollPosition = window.pageYOffset;
+
+
+    if (prevScrollPosition < actualScrollPosition || actualScrollPosition > 265) {
+        document.getElementById('nav').style.height = '0em';
+        document.getElementById('logo').style.display = 'none';
+
+
+
+    } else {
+        document.getElementById('nav').style.height = '7em';
+
+        document.getElementById('logo').style.display = 'block';
+
+    }
+
+    prevScrollPosition = actualScrollPosition;
+}
