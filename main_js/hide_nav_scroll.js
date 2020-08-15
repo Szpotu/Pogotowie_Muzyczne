0a96a9ab643280b3cde45980e833fdea5606334d
Szpotsu <szpots.dev@gmail.com>
2020-08-15 17:08:08 +0200

    Added hiding-scroll behavings of header nav. Changed color concept to complementary well-contrasted (1:6.3), added language shifter button, created and positioned test layout of contact side bar

--- main_js/hide_nav_scroll.js
+++ main_js/hide_nav_scroll.js
@@ -1,20 +1,44 @@
 let prevScrollPosition = window.pageYOffset; // global var - previous scroll px
 
+const qSelector = document.querySelector('btnNav');
+
+
+const cache_element = function(htmlElement) {
+    return document.getElementById(htmlElement);
+}
+const changeClass = function(className, element) {
+    let firstClass = document.getElementById(element);
+    firstClass.className = className;
+}
 
 window.onscroll = () => {
+
     let actualScrollPosition = window.pageYOffset;
 
 
     if (prevScrollPosition < actualScrollPosition || actualScrollPosition > 265) {
-        document.getElementById('nav').style.height = '0em';
-        document.getElementById('logo').style.display = 'none';
 
 
+        changeClass('shrinkNavBtn', 'navButton1');
+        changeClass('shrinkNavBtn', 'navButton2');
+        changeClass('shrinkNavBtn', 'navButton3');
+        changeClass('shrinkNavBtn', 'navButton4');
+        changeClass('shrinkNavBtn', 'navButton5');
+        changeClass('scrolled-nav', 'nav');
+        cache_element('logo').style.opacity = 0;
+        cache_element('language-shifter').style.opacity = 0;
+
+        //Loop
 
     } else {
-        document.getElementById('nav').style.height = '7em';
-
-        document.getElementById('logo').style.display = 'block';
+        cache_element('language-shifter').style.opacity = 1;
+        document.getElementById('logo').style.opacity = 1;
+        changeClass('navBtn', 'navButton1');
+        changeClass('navBtn', 'navButton2');
+        changeClass('navBtn', 'navButton3');
+        changeClass('navBtn', 'navButton4');
+        changeClass('navBtn', 'navButton5');
+        changeClass('basic-nav', 'nav');
 
     }
 
