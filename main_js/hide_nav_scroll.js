f0a67790ac59016bf11e0c515a89af048283eba4
Szpotsu <szpots.dev@gmail.com>
2020-08-18 00:16:31 +0200

    Added responsivness to header elements,

--- main_js/hide_nav_scroll.js
+++ main_js/hide_nav_scroll.js
@@ -19,11 +19,6 @@ window.onscroll = () => {
     if (prevScrollPosition < actualScrollPosition || actualScrollPosition > 265) {
 
 
-        changeClass('shrinkNavBtn', 'navButton1');
-        changeClass('shrinkNavBtn', 'navButton2');
-        changeClass('shrinkNavBtn', 'navButton3');
-        changeClass('shrinkNavBtn', 'navButton4');
-        changeClass('shrinkNavBtn', 'navButton5');
         changeClass('scrolled-nav', 'nav');
         cache_element('logo').style.opacity = 0;
         cache_element('language-shifter').style.opacity = 0;
@@ -33,11 +28,7 @@ window.onscroll = () => {
     } else {
         cache_element('language-shifter').style.opacity = 1;
         document.getElementById('logo').style.opacity = 1;
-        changeClass('navBtn', 'navButton1');
-        changeClass('navBtn', 'navButton2');
-        changeClass('navBtn', 'navButton3');
-        changeClass('navBtn', 'navButton4');
-        changeClass('navBtn', 'navButton5');
+
         changeClass('basic-nav', 'nav');
 
     }
