6fdd7205380712f18de59281f115c3cb28fee3c3
Szpotsu <szpots.dev@gmail.com>
2020-08-23 19:44:53 +0200

    Customize and update of Gridbox( Site-3 ). Redesigned main navigation, switched hide_nav_scroll function into simple CSS classname change (so fix transition effext)

--- main_js/hide_nav_scroll.js
+++ main_js/hide_nav_scroll.js
@@ -1,27 +1,29 @@
 let prevScrollPosition = window.pageYOffset; // global var - previous scroll px
 const qSelector = document.querySelector('btnNav');
-
 const cache_element = function(htmlElement) {
     return document.getElementById(htmlElement);
 }
-const changeClass = function(className, element) {
+const changeClassOfElement = function(className, element) {
     let firstClass = document.getElementById(element);
     firstClass.className = className;
 }
 
 window.onscroll = () => {
 
-    let actualScrollPosition = window.pageYOffset;
+        let actualScrollPosition = window.pageYOffset;
+        if (prevScrollPosition < actualScrollPosition || actualScrollPosition > 250) {
 
-    if (prevScrollPosition < actualScrollPosition || actualScrollPosition > 250) {
+            cache_element('nav').className = 'scrolled-nav';
+            cache_element('logo').style.opacity = 0;
+            cache_element('navBtn').className = 'shrinked-nav.btn';
 
-        changeClass('scrolled-nav', 'nav');
-        cache_element('logo').style.opacity = 0;
-    } else {
 
-        document.getElementById('logo').style.opacity = 1;
-        changeClass('basic-nav', 'nav');
+        } else {
+            cache_element('logo').style.opacity = 1;
+            cache_element('nav').className = 'basic-nav';
+        }
+
+        prevScrollPosition = actualScrollPosition;
     }
-
-    prevScrollPosition = actualScrollPosition;
-}
+    // Class change for scroll nav
+    // Remove html elements while hamburger visible
